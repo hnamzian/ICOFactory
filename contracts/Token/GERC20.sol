1576a0cca328e3826e31a9975a7c7f2f5d62ac9d
hnamzian <namazian87@gmail.com>
2019-05-04 11:17:53 +0430

    import ERC20Pausable contract from openzeppelin

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract GERC20 {
 
