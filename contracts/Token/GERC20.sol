d7ce82fbad194b636d3fe64f96f898f800cb435e
hnamzian <namazian87@gmail.com>
2019-05-04 11:19:57 +0430

    import TokenTimelock contract from openzeppelin

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -5,6 +5,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
 contract GERC20 {
 
