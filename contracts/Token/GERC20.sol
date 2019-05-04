74ca86550efddbd4a75f6835b7c9ba7afd89f5df
hnamzian <namazian87@gmail.com>
2019-05-04 11:18:40 +0430

    import ERC20Capped contract from openzeppelin

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -4,6 +4,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 
 contract GERC20 {
 
