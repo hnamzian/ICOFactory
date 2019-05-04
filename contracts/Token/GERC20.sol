e29829e6f65c040eca5b66522081f917e16d11b4
hnamzian <namazian87@gmail.com>
2019-05-04 11:17:19 +0430

    import ERC20Mintable contract from openzeppelin

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.2;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract GERC20 {
 
