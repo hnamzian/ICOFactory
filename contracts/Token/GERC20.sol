61513486d89f0c697143fa1430c5a387eecb5b59
hnamzian <namazian87@gmail.com>
2019-05-06 00:14:11 +0430

    add GERC20 config flags isMintable, isBurnable, isPausable, isCapped

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -7,6 +7,16 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
-contract GERC20 {
+contract GERC20 is ERC20, 
+  ERC20Burnable,
+  ERC20Mintable,
+  ERC20Pausable,
+  ERC20Capped
+{
+  bool isMintable;
+  bool isBurnable;
+  bool isPausable;
+  bool isCapped;
 
+  constructor(uint256 _cap) ERC20Capped(cap) public {}
 }
