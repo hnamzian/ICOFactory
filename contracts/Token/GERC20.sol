55ca179177ccb8aa6b1ca9eaf4daea290e3b5178
hnamzian <namazian87@gmail.com>
2019-05-22 11:53:51 +0430

    remove TokenTimelock from import list

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -5,7 +5,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
 contract GERC20 is ERC20, 
   ERC20Burnable,
