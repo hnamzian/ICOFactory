038dd576abb61a067d8454628114f35af1e3ea27
hnamzian <namazian87@gmail.com>
2019-05-25 13:14:20 +0430

    add initialization of totalSupply into constructor

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -26,6 +26,7 @@ contract GERC20 is ERC20,
       uint256 cap
     ) ERC20Capped(cap) public {
         if (!isMintable) require(totalSupply > 0, "totalSupply must be greater than 0");
+        _totalSupply = totalSupply;
         _isMintable = isMintable;
         _isBurnable = isBurnable;
         _isPausable = isPausable;
