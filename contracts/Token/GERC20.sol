82ab0a3586942ca41dacfaa85223212e5e0a749a
hnamzian <namazian87@gmail.com>
2019-05-25 14:48:53 +0430

    derive GERC20 from ERC20Detailed

--- contracts/Token/GERC20.sol
+++ contracts/Token/GERC20.sol
@@ -5,8 +5,10 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract GERC20 is ERC20,
+  ERC20Detailed,
   ERC20Burnable,
   ERC20Mintable,
   ERC20Pausable,
