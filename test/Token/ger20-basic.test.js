53d28fa2654742ec6e6e5038f86dccd294378704
hnamzian <namazian87@gmail.com>
2019-05-26 17:23:53 +0430

    test mint procedure

--- test/Token/ger20-basic.test.js
+++ test/Token/ger20-basic.test.js
@@ -1,1 +1,28 @@
+const GERC20 = artifacts.require("./Token/GERC20");
 
+const tokenArgs = {
+  name: "JIMMIX",
+  symbol: "JMX",
+  decimals: 3,
+  isBurnable: true,
+  isPausable: true,
+  isCapped: true,
+  cap: 10000000
+};
+
+contract("GERC20", accounts => {
+  it("should mint token for account[1]", async () => {
+    let token = await GERC20.deployed(
+      tokenArgs.name,
+      tokenArgs.symbol,
+      tokenArgs.decimals,
+      tokenArgs.isBurnable,
+      tokenArgs.isPausable,
+      tokenArgs.isCapped,
+      tokenArgs.cap
+    );
+    await token.mint(accounts[1], 1, { from: accounts[0] });
+    const balance = await token.balanceOf(accounts[1]);
+    assert.equal(+balance, 1);
+  });
+});
