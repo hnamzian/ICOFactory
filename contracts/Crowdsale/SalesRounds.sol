74d60772328c1c3fd59b02f6ad1e62ee609b8c37
hnamzian <namazian87@gmail.com>
2019-05-28 16:02:40 +0430

    add Round struct containing Round specifications

--- contracts/Crowdsale/SalesRounds.sol
+++ contracts/Crowdsale/SalesRounds.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.5.2;
 
 contract SalesRounds {
-
+  struct Round {
+    uint256 opening;  // opening timestamp of Round
+    uint256 ending;   // ending timestamp of Round
+    uint256 tokenPerEth;  // amount of tokens in decimals per Ether
+    uint256 tokenCap;   // max number of tokens allowed to be purchased at Round
+    uint256 minTokensInvest;  // minimum amount of tokens to be purchased individually
+    uint256 maxTokensInvest;  // maximum amount of tokens can be purchased individually
+  }
+  
 }
