dc763e5005a5d6307c073365158ebb9d833d5924
hnamzian <namazian87@gmail.com>
2019-06-01 15:48:25 +0430

    add refund() sends back invests to investor at Refunding state

--- contracts/Crowdsale/RefundableCrowdsale.sol
+++ contracts/Crowdsale/RefundableCrowdsale.sol
@@ -3,5 +3,15 @@ pragma solidity "0.5.2";
 import "./FinalizableCrowdsale.sol";
 
 contract RefundableCrowdsale is FinalizableCrowdsale {
-  
+  event Refunded(address investor, uint256 invest);
+    
+  function refund(address payable investor) public {
+    require(isFinalized());
+    require(state == State.Refunding);
+    require(_invests[investor] != 0);
+    uint256 refundValue = _invests[investor];
+    _invests[investor] = 0;
+    investor.transfer(refundValue);
+    emit Refunded(investor, refundValue);
+  }
 }
