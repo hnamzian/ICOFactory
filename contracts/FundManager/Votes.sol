b395a8ff797fa2e96f47d5aaed17d9c1ad703743
hnamzian <namazian87@gmail.com>
2019-06-01 22:00:57 +0430

    change election prefixes to voting

--- contracts/FundManager/Votes.sol
+++ contracts/FundManager/Votes.sol
@@ -4,12 +4,12 @@ import "./ProjectOwnerRole.sol";
 import "./WhitelistedOracles.sol";
 
 contract Voting is ProjectOwnerRole, WhitelistedOracles {
-  enum ElectionType {Funding, CloseProject}
-  enum ElectionState {Voting, Accepted, Denied}
+  enum VotingType {Funding, CloseProject}
+  enum VotingState {Voting, Accepted, Denied}
 
   struct VotingSession {
-    ElectionType electioType;
-    ElectionState state;
+    VotingType votingType;
+    VotingState state;
     address createdBy;
     mapping (address => bool) voteOf;
     uint8 positiveVotes;
