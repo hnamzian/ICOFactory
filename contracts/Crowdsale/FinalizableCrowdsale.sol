643938b14fc30f68b8e6ea14e4b2a8e84e91b5dc
hnamzian <namazian87@gmail.com>
2019-06-01 15:14:35 +0430

    add finalize() sets crowdsale state + add constructor

--- contracts/Crowdsale/FinalizableCrowdsale.sol
+++ contracts/Crowdsale/FinalizableCrowdsale.sol
@@ -9,5 +9,16 @@ contract FinalizableCrowdsale is BasicCrowdsale {
   // Finalized: closed and fund raised successfully 
   enum State {Running, Refunding, Finalized}
   State private state;
+
+  constructor() public {
+    state = State.Running;
+  }
   
+  function finalize() public onlyOwner {
+    if (etherRaised > hardcap) state = State.Finalized;
+    else if (isSalesClosed()) {
+      if (etherRaised >= softcap) state = State.Finalized;
+      else state = State.Refunding;
+    }
+  }
 }
