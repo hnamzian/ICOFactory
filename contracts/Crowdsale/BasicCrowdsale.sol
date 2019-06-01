fd11dcf0671e10256204120f4bb41d07551ae526
hnamzian <namazian87@gmail.com>
2019-06-01 14:08:01 +0430

    add _token of type GERC20 token to states

--- contracts/Crowdsale/BasicCrowdsale.sol
+++ contracts/Crowdsale/BasicCrowdsale.sol
@@ -1,11 +1,14 @@
 pragma solidity "0.5.2";
 
+import "../Token/GERC20.sol";
 import "./SalesRounds.sol";
 import "./WhitelistedInvestors.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract BasicCrowdsale is SalesRounds, WhitelistedInvestors {
   using SafeMath for uint256;
+
+  GERC20 private _token;
 
   uint256 private _softcap;
   uint256 private _hardcap;
