d6ff01022d6b0d9f5872f9f01007e995f9636082
hnamzian <namazian87@gmail.com>
2019-06-02 12:55:56 +0430

    add and update number of oracles registered

--- contracts/FundManager/WhitelistedOracles.sol
+++ contracts/FundManager/WhitelistedOracles.sol
@@ -2,14 +2,18 @@ pragma solidity "0.5.2";
 
 import "openzeppelin-solidity/contracts/access/Roles.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract WhitelistedOracles is Ownable {
+  using SafeMath for uint256;
   using Roles for Roles.Role;
 
   event OracleAdded(address indexed account);
   event OracleRemoved(address indexed account);
 
   Roles.Role private _Oracles;
+
+  uint256 _numberOfOracles;
 
   modifier onlyOracle() {
     require(isOracle(msg.sender));
@@ -30,11 +34,13 @@ contract WhitelistedOracles is Ownable {
 
   function _addOracle(address account) internal {
     _Oracles.add(account);
+    _numberOfOracles = _numberOfOracles.add(1);
     emit OracleAdded(account);
   }
 
   function _removeOracle(address account) internal {
     _Oracles.remove(account);
+    _numberOfOracles = _numberOfOracles.sub(1);
     emit OracleRemoved(account);
   }
   
