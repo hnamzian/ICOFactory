87f9ed311c9e49f9f330ec400fe9969994506bb3
hnamzian <namazian87@gmail.com>
2019-06-08 13:56:38 +0430

    change _terminationManager to _terminator

--- contracts/Crowdsale/TerminatorRole.sol
+++ contracts/Crowdsale/TerminatorRole.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract TerminatorRole is Ownable {
-  address private _terminationManger;
+  address private _terminator;
 
   modifier onlyTerminator() {
-    require(msg.sender == _terminationManger, "you are not permitted");
+    require(msg.sender == _terminator, "you are not permitted");
     _;
   }
 
-  function setTerminatationManager(address terminationManager) public onlyOwner {
-    _terminationManger = terminationManager;
+  function setTerminatationManager(address terminator) public onlyOwner {
+    _terminator = terminator;
   }
 }
