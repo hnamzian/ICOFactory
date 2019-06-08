8ee57902104e7a96261d6fb8e87ee1311f6aa2cc
hnamzian <namazian87@gmail.com>
2019-06-08 14:21:37 +0430

    add Terminated condition to refund

--- contracts/Crowdsale/RefundableCrowdsale.sol
+++ contracts/Crowdsale/RefundableCrowdsale.sol
@@ -1,15 +1,16 @@
 pragma solidity "0.5.2";
 
 import "./FinalizableCrowdsale.sol";
+import "./WithdrawalCrowdsale.sol";
 
-contract RefundableCrowdsale is FinalizableCrowdsale {
+contract RefundableCrowdsale is FinalizableCrowdsale, WithdrawalCrowdsale {
   event Refunded(address investor, uint256 invest);
     
   function refund(address payable investor) public {
-    require(state == State.Refunding);
+    require(state == State.Refunding  || state == State.Terminated);
     require(_invests[investor] != 0);
 
-    uint256 refundValue = _invests[investor];
+    uint256 refundValue =  _etherRaised.sub(_totalWithdrawals).mul(_invests[investor]).div(_etherRaised);
     _invests[investor] = 0;
     investor.transfer(refundValue);
     
