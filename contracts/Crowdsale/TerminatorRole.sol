a5b5b2b14b747381e1a5ec68ac0db76cf113e4cf
hnamzian <namazian87@gmail.com>
2019-06-08 13:54:08 +0430

    create initial TerminatorRole contract + add setTerminatationManager()

---
+++ contracts/Crowdsale/TerminatorRole.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.2;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract TerminatorRole is Ownable {
+  address private _terminationManger;
+
+  function setTerminatationManager(address terminationManager) public onlyOwner {
+    _terminationManger = terminationManager;
+  }
+}
