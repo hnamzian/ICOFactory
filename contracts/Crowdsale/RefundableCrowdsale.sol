97934ae071f98ae0c5b751ca97656cde39d241ad
hnamzian <namazian87@gmail.com>
2019-06-08 14:08:16 +0430

    remove isFinalized() condition for refunding

--- contracts/Crowdsale/RefundableCrowdsale.sol
+++ contracts/Crowdsale/RefundableCrowdsale.sol
@@ -6,12 +6,13 @@ contract RefundableCrowdsale is FinalizableCrowdsale {
   event Refunded(address investor, uint256 invest);
     
   function refund(address payable investor) public {
-    require(isFinalized());
     require(state == State.Refunding);
     require(_invests[investor] != 0);
+
     uint256 refundValue = _invests[investor];
     _invests[investor] = 0;
     investor.transfer(refundValue);
+    
     emit Refunded(investor, refundValue);
   }
 }
