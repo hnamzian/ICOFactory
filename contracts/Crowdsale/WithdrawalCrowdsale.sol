d78f29949b4c2d1752d4b0916dec0a8efc32df60
hnamzian <namazian87@gmail.com>
2019-06-08 11:07:44 +0430

    add withdrawalAddress function

--- contracts/Crowdsale/WithdrawalCrowdsale.sol
+++ contracts/Crowdsale/WithdrawalCrowdsale.sol
@@ -6,6 +6,8 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract WithdrawalCrowdsale is BasicCrowdsale, Ownable {
   address private _withdrawManager;
 
+  address private _withdrawalAddress;
+
   modifier onlyWithdrawManger() {
     require(msg.sender == _withdrawManager, "you are not permitted");
     _;
@@ -14,4 +16,10 @@ contract WithdrawalCrowdsale is BasicCrowdsale, Ownable {
   function setWithdrawManager(address withdrawManager) public onlyOwner {
     _withdrawManager = withdrawManager;
   }
+
+  function setWithdrawalAddress(address withdrawalAddress) public onlyOwner {
+    _withdrawalAddress = withdrawalAddress;
+  }
+
+  
 }
