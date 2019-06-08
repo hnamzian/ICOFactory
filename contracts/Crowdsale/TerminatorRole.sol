30ad6d164313080c1c36263166276ede8c5985b5
hnamzian <namazian87@gmail.com>
2019-06-08 13:55:41 +0430

    add onlyTerminator modifier

--- contracts/Crowdsale/TerminatorRole.sol
+++ contracts/Crowdsale/TerminatorRole.sol
@@ -5,6 +5,11 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract TerminatorRole is Ownable {
   address private _terminationManger;
 
+  modifier onlyTerminator() {
+    require(msg.sender == _terminationManger, "you are not permitted");
+    _;
+  }
+
   function setTerminatationManager(address terminationManager) public onlyOwner {
     _terminationManger = terminationManager;
   }
