1640d6e80e1a4ea0aa6c9db9712c6d3371f1c433
hnamzian <namazian87@gmail.com>
2019-06-10 18:47:15 +0430

    emit event at launching contract

--- contracts/Launcher/TokenLauncher.sol
+++ contracts/Launcher/TokenLauncher.sol
@@ -4,6 +4,16 @@ import "../Token/GERC20.sol";
 
 contract TokenLauncher {
 
+  event TokenLaunched(
+  address tokenAddress,
+  string name,
+  string symbol,
+  uint8 decimals,
+  bool isBurnable,
+  bool isPausable,
+  bool isCapped,
+  uint256 cap);
+
   function launchToken(
     string memory name,
     string memory symbol,
@@ -12,7 +22,10 @@ contract TokenLauncher {
     bool isPausable,
     bool isCapped,
     uint256 cap) public {
-    new GERC20(name, symbol, decimals, isBurnable, isPausable, isCapped, cap);
+    GERC20 gerc20 = new GERC20(name, symbol, decimals, isBurnable, isPausable, isCapped, cap);
+
+    emit TokenLaunched(address(gerc20), name, symbol, decimals, isBurnable, isPausable, isCapped, cap);
+
   }
 
 }
