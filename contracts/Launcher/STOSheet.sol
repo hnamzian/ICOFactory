66eeb2946b8386cb609aa904b23b7b96f5567f8f
hnamzian <namazian87@gmail.com>
2019-06-18 15:46:58 +0430

    add Token, Crowdsale, Voting struct

--- contracts/Launcher/STOSheet.sol
+++ contracts/Launcher/STOSheet.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.5.2;
 
 contract STOSheet {
-  
+  struct Token {
+    string name;
+    string symbol;
+    uint8 decimals;
+    bool isBurnable;
+    bool isPausable;
+    bool isCapped;
+    uint256 cap;
+    address tokenAddress;
+  }
+
+  struct Crowdsale {
+    uint256 softcap;
+    uint256 hardcap;
+    uint256 maxIndividualEtherInvest;
+    address crowdsaleAddress;
+  }
+
+  struct Voting {
+    address votingAddress;
+  }
+
 }
