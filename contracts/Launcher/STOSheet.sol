d50f7b7701480e4552ac9162a8053c41c4f432fa
hnamzian <namazian87@gmail.com>
2019-06-18 17:30:41 +0430

    import strutil library to use ctrconcat functoin in setSTOConfigs

--- contracts/Launcher/STOSheet.sol
+++ contracts/Launcher/STOSheet.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.2;
 
+import "../utils/strutil.sol";
+
 contract STOSheet {
+  using strutil for string;
+  
   struct Token {
     string name;
     string symbol;
@@ -31,5 +35,21 @@ contract STOSheet {
   }
 
   mapping(string => STO) private STOs;
+
+  function setSTOConfigs(
+    string memory name,
+    string memory symbol,
+    uint8 decimals,
+    bool isBurnable,
+    bool isPausable,
+    bool isCapped,
+    uint256 cap,
+    uint256 softcap,
+    uint256 hardcap,
+    uint256 maxIndividualEtherInvest) public returns (string memory id) {
+    id = keccak256(block.timestamp, msg.sender);
+
+
+  }
 
 }
