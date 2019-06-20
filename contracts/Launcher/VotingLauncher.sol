f7d3bed76edbeef0e9565e4353e38adc4664e0f9
hnamzian <namazian87@gmail.com>
2019-06-20 14:45:13 +0430

    add _STOSheet as interface of STOSheet and set address at constructor

--- contracts/Launcher/VotingLauncher.sol
+++ contracts/Launcher/VotingLauncher.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.5.2;
 import "../FundManager/Voting.sol";
 
 contract VotingLauncher {
+  STOSheet _STOSheet;
+
   event VotingLaunched(address votingAddress);
 
+  constructor(address STOSheetAddress) public {
+    _STOSheet = STOSheet(STOSheetAddress);
+  }
+  
   function launchVoting(address crowdsaleAddress) public returns (address) {
     Voting voting = new Voting(crowdsaleAddress);
 
