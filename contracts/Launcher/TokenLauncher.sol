1deda82ba78a10fd8b02db558e8ce0318b2763b3
hnamzian <namazian87@gmail.com>
2019-06-20 14:38:54 +0430

    get token configs from STOSheet contract to launch new GERC20

--- contracts/Launcher/TokenLauncher.sol
+++ contracts/Launcher/TokenLauncher.sol
@@ -21,15 +21,18 @@ contract TokenLauncher {
     _STOSheet = STOSheet(STOSheetAddress);
   }
 
-  function launchToken(
-    string memory name,
+  function launchToken(string memory stoID) public returns (address) {
+    (string memory name,
     string memory symbol,
     uint8 decimals,
     bool isBurnable,
     bool isPausable,
     bool isCapped,
-    uint256 cap) public returns (address) {
+    uint256 cap,
+    address tokenAddress) = _STOSheet.getTokenConfigs(stoID);
+    
     GERC20 gerc20 = new GERC20(name, symbol, decimals, isBurnable, isPausable, isCapped, cap);
+    tokenAddress = address(gerc20);
 
     emit TokenLaunched(address(gerc20), name, symbol, decimals, isBurnable, isPausable, isCapped, cap);
 
