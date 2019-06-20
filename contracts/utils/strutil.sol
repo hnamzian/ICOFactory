90ec9052347bde86f7cf17b478afd44d3516cd9a
hnamzian <namazian87@gmail.com>
2019-06-20 15:11:21 +0430

    change state mutability to pure

--- contracts/utils/strutil.sol
+++ contracts/utils/strutil.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.2;
 
 library strutil {
-  function concat(string memory str1, string memory str2) public returns (string memory) {
+  function concat(string memory str1, string memory str2) public pure returns (string memory) {
     bytes memory str1Bytes = bytes(str1);
     bytes memory str2Bytes = bytes(str2);
     string memory mergedString = new string(str1Bytes.length + str2Bytes.length);
