2b195fcdfa179581368e6775d376a4ad16401ce7
hnamzian <namazian87@gmail.com>
2019-06-20 15:10:58 +0430

    change state mutability of functions to pure

--- contracts/utils/bytesUtil.sol
+++ contracts/utils/bytesUtil.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.2;
 
 library bytesUtils {
-  function bytes32ToBytes(bytes32 _bytes32) public view returns (bytes memory) {
+  function bytes32ToBytes(bytes32 _bytes32) public pure returns (bytes memory) {
     bytes memory bytesArray = new bytes(32);
     for (uint256 i; i < 32; i++) {
       bytesArray[i] = _bytes32[i];
@@ -9,7 +9,7 @@ library bytesUtils {
     return bytesArray;
   }
 
-  function bytes32ToString(bytes32 _bytes32) public view returns (string memory) {
+  function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
     bytes memory bytesArray = bytes32ToBytes(_bytes32);
     return string(bytesArray);
   }
