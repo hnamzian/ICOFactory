96df838bf88abb14538df8f9f7b702ebfc97bd68
hnamzian <namazian87@gmail.com>
2019-06-22 17:45:15 +0430

    remove contract deployment at each iteration as will be deployed by beforeEach routine

--- test/Token/gerc20-details.test.js
+++ test/Token/gerc20-details.test.js
@@ -25,27 +25,16 @@ contract("GERC20", accounts => {
   });
   
   it("should check token name", async () => {
-    let token = await GERC20.deployed(
-      tokenArgs.name,
-      tokenArgs.symbol,
-      tokenArgs.decimals,
-      tokenArgs.isBurnable,
-      tokenArgs.isPausable,
-      tokenArgs.isCapped,
-      tokenArgs.cap
-    );
     const name = await token.name.call();
     assert.equal(name, tokenArgs.name);
   });
 
   it("should check token symbol", async () => {
-    let token = await GERC20.deployed();
     const symbol = await token.symbol.call();
     assert.equal(symbol, tokenArgs.symbol);
   });
 
   it("should check token decimals", async () => {
-    let token = await GERC20.deployed();
     const decimals = await token.decimals.call();
     assert.equal(decimals, tokenArgs.decimals);
   });
