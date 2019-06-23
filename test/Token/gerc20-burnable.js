69b202437d5064bb6e27f876a2596d5a9781b412
hnamzian <namazian87@gmail.com>
2019-06-23 12:36:32 +0430

    add gerc20-burnable.js to test token burning functions

---
+++ test/Token/gerc20-burnable.js
@@ -0,0 +1,25 @@
+const GERC20 = artifacts.require("./Token/GERC20");
+
+const tokenArgs = {
+  name: "JIMMIX",
+  symbol: "JMX",
+  decimals: 3,
+  isPausable: true,
+  isCapped: true,
+  cap: 10000000
+};
+
+contract("GERC20", accounts => {
+  let token;
+
+  beforeEach(async () => {
+    token = await GERC20.new(
+      tokenArgs.name,
+      tokenArgs.symbol,
+      tokenArgs.decimals,
+      tokenArgs.isPausable,
+      tokenArgs.isCapped,
+      tokenArgs.cap
+    );
+  });
+})
