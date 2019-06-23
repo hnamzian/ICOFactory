784a97c157475483258ab288bac8570390969404
hnamzian <namazian87@gmail.com>
2019-06-23 13:51:50 +0430

    test reversion of burning tokens more than owner's balance

--- test/Token/gerc20-burnable.js
+++ test/Token/gerc20-burnable.js
@@ -30,6 +30,19 @@ contract("GERC20", accounts => {
     assert.equal(+balance, 0);
   });
 
+  it("should revert burning tokens more than owner's balance", async () => {
+    await token.mint(accounts[1], 1, { from: accounts[0] });
+    try {
+      await token.burn(2, { from: accounts[1] });
+    } catch (ex) {
+      revert = ex.message.includes("revert");
+    }
+    assert.equal(revert, true);
+    
+    const balance = await token.balanceOf(accounts[1]);
+    assert.equal(+balance, 1);
+  });
+
   it("should burn approved tokens by allowed account", async () => {
     await token.mint(accounts[1], 1, { from: accounts[0] });
     await token.approve(accounts[2], 1, { from: accounts[1] });
@@ -39,4 +52,5 @@ contract("GERC20", accounts => {
     assert.equal(+balance, 0);
     assert.equal(+allowance, 0);
   });
+  
 });
