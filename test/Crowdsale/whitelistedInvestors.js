e8093484fb7e86021e66f3b47a987f3fe4f5b53f
hnamzian <namazian87@gmail.com>
2019-06-24 18:03:04 +0430

    test removing investor from whitelist

--- test/Crowdsale/whitelistedInvestors.js
+++ test/Crowdsale/whitelistedInvestors.js
@@ -6,4 +6,17 @@ contract("WhitelistedInvestors", accounts => {
   beforeEach(async () => {
     whitelistedInvestors = await WhitelistedInvestors.new();
   });
+
+  it("should add investor to whitelist", async () => {
+    await whitelistedInvestors.addInvestor(accounts[1], { from: accounts[0] });
+    const isInvestor = await whitelistedInvestors.isInvestor(accounts[1]);
+    assert.isTrue(isInvestor);
+  });
+
+  it("should remove investor to whitelist", async () => {
+    await whitelistedInvestors.addInvestor(accounts[1], { from: accounts[0] });
+    await whitelistedInvestors.removeInvestor(accounts[1], { from: accounts[0] });
+    const isInvestor = await whitelistedInvestors.isInvestor(accounts[1]);
+    assert.isFalse(isInvestor);
+  });
 });
