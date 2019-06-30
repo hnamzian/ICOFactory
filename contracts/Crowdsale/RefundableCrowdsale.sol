dc0f4f8808d65fb0c06c6cbe867795751b77e4bb
hnamzian <namazian87@gmail.com>
2019-06-30 12:37:16 +0430

    add error comment on invalid time to refund invest

--- contracts/Crowdsale/RefundableCrowdsale.sol
+++ contracts/Crowdsale/RefundableCrowdsale.sol
@@ -6,7 +6,7 @@ contract RefundableCrowdsale is FinalizableCrowdsale {
   event Refunded(address investor, uint256 invest);
     
   function refund(address payable investor) public {
-    require(state == State.Refunding  || state == State.Terminated);
+    require(state == State.Refunding || state == State.Terminated, "Refunding state has not been activated");
     require(_invests[investor] != 0, "Amount of remaining invest is zero");
 
     uint256 refundValue = _etherRaised.sub(_totalWithdrawals).mul(_invests[investor]).div(_etherRaised);
