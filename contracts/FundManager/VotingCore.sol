a766a7a7281269a34752d36fe3e3fa957f09b86d
hnamzian <namazian87@gmail.com>
2019-06-30 16:28:49 +0430

    add VotingSession struct containing basic information of a voting

--- contracts/FundManager/VotingCore.sol
+++ contracts/FundManager/VotingCore.sol
@@ -4,4 +4,15 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract VotingCore {
   using SafeMath for uint256;
+
+  enum VotingState {Accepted, Denied}
+
+  struct VotingSession {
+    VotingState state;
+    bool finalized;
+    address createdBy;
+    uint256 ending;
+    mapping (address => bool) voteOf;
+    uint256 positiveVotes;
+  }
 }
