337e1ef625f177030895afb6192470d7af0ea6e0
hnamzian <namazian87@gmail.com>
2019-06-30 19:33:14 +0430

    add necessary states and struct

--- contracts/FundManager/CloseProjectVoting.sol
+++ contracts/FundManager/CloseProjectVoting.sol
@@ -4,5 +4,11 @@ import "./WhitelistedOracles.sol";
 import "./VotingCore.sol";
 
 contract CloseProjectVoting is VotingCore, WhitelistedOracles {
+  struct CloseProjectVoting {
+    string message;
+  }
+  mapping (string => CloseProjectVoting) closeProjectVotings;
+
+  uint256 internal minVotesToCloseProject;
 
 }
