cf91ff8324d8ee59337cae2c4694546fb7fa981b
hnamzian <namazian87@gmail.com>
2019-07-01 11:47:09 +0430

    remove states and functions implemented by FundRaisingVoting and CloseProjectVoting contracts

--- contracts/FundManager/Voting.sol
+++ contracts/FundManager/Voting.sol
@@ -10,33 +10,6 @@ contract Voting is ProjectOwnerRole, WhitelistedOracles {
 
   GeneralCrowdsale Crowdsale;
 
-  enum VotingState {Accepted, Denied}
-
-  struct VotingSession {
-    VotingState state;
-    bool finalized;
-    address createdBy;
-    uint256 ending;
-    mapping (address => bool) voteOf;
-    uint256 positiveVotes;
-  }
-
-  struct FundVoting {
-    VotingSession votingSession;
-    uint256 requestedFund;
-    string message;
-  }
-
-  struct CloseProjectVoting {
-    VotingSession votingSession;
-    string message;
-  }
-
-  FundVoting[] fundVoting;
-  CloseProjectVoting[] closeProjectVoting;
-
-  uint256 private _minVotes;
-
   constructor(address crowdsaleAddress) public {
     Crowdsale = GeneralCrowdsale(crowdsaleAddress);
   }
@@ -54,80 +27,6 @@ contract Voting is ProjectOwnerRole, WhitelistedOracles {
     _;
   }
 
-  function requestFundVoting(uint256 fund, uint256 ending, string memory message) public onlyProjectOwner {
-    FundVoting memory lastFundVoting;
-    if (fundVoting.length > 0) {
-      lastFundVoting = fundVoting[fundVoting.length-1];
-      require(block.timestamp > lastFundVoting.votingSession.ending, "another voting session is still running");
-    }
-    require(fund > 0, "requested fund must be grater than 0");
-
-    FundVoting memory _fundVoting = FundVoting({
-      votingSession: VotingSession({
-        state: VotingState.Denied,
-        finalized: false,
-        ending: ending,
-        createdBy: msg.sender,
-        positiveVotes: 0
-      }),
-      requestedFund: fund,
-      message: message
-    });
-
-    fundVoting.push(_fundVoting);
-  }
-
-  function requestCloseProjectVoting(uint256 ending, string memory message) public onlyOracle {
-    CloseProjectVoting memory lastVoting;
-    if (closeProjectVoting.length > 0) {
-      lastVoting = closeProjectVoting[closeProjectVoting.length-1];
-      require(block.timestamp > lastVoting.votingSession.ending, "another voting session is still running");
-    }
-
-    CloseProjectVoting memory _closeProjectVoting = CloseProjectVoting({
-      votingSession: VotingSession({
-        state: VotingState.Denied,
-        finalized: false,
-        ending: ending,
-        createdBy: msg.sender,
-        positiveVotes: 0
-      }),
-      message: message
-    });
-
-    closeProjectVoting.push(_closeProjectVoting);
-  }
-
-  function voteFundRaising(bool _vote) public onlyOracle onlyFundVotingRunning {
-    require(fundVoting.length > 0, "No voting session exists");
-    VotingSession storage _lastVoting = fundVoting[fundVoting.length-1].votingSession;
-    if(_lastVoting.voteOf[msg.sender] != _vote) {
-      _lastVoting.voteOf[msg.sender] = _vote;
-      if(_vote) _lastVoting.positiveVotes = _lastVoting.positiveVotes.add(1);
-      else _lastVoting.positiveVotes = _lastVoting.positiveVotes.sub(1);
-      _lastVoting.state = _voteConsensus(_lastVoting.positiveVotes);
-    }
-  }
-
-  function voteCloseProject(bool _vote) public onlyOracle onlyCloseProjectVotingRunning {
-    require(closeProjectVoting.length > 0, "No voting session exists");
-    VotingSession storage _lastVoting = closeProjectVoting[closeProjectVoting.length-1].votingSession;
-    if(_lastVoting.voteOf[msg.sender] != _vote) {
-      _lastVoting.voteOf[msg.sender] = _vote;
-      if(_vote) _lastVoting.positiveVotes = _lastVoting.positiveVotes.add(1);
-      else _lastVoting.positiveVotes = _lastVoting.positiveVotes.sub(1);
-      _lastVoting.state = _voteConsensus(_lastVoting.positiveVotes);
-    }
-  }
-
-  function getFundVotingStatus(uint votingIndex) public view returns (VotingState) {
-    return fundVoting[votingIndex].votingSession.state;
-  }
-
-  function getCloseVotingStatus(uint votingIndex) public view returns (VotingState) {
-    return closeProjectVoting[votingIndex].votingSession.state;
-  }
-
   function claimFund(uint256 votingIndex) public onlyProjectOwner returns (uint256) {
     FundVoting storage _lastVoting = fundVoting[votingIndex];
     require(block.timestamp > _lastVoting.votingSession.ending, "Voting is still running");
@@ -143,11 +42,6 @@ contract Voting is ProjectOwnerRole, WhitelistedOracles {
     CloseProjectVoting memory _lastVoting = closeProjectVoting[closeProjectVoting.length-1];
     require(block.timestamp > _lastVoting.votingSession.ending, "voting is still running");
     if(_lastVoting.votingSession.state == VotingState.Accepted) Crowdsale.terminateProject();
-  }
-
-  function _voteConsensus(uint256 positiveVotes) internal view returns (VotingState) {
-    if (positiveVotes >= _minVotes) return VotingState.Accepted;
-    return VotingState.Denied;
   }
 
 }
