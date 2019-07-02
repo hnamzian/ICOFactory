0b0589af286905193145908f49db92ce4d4f4edb
hnamzian <namazian87@gmail.com>
2019-07-02 17:35:02 +0430

    test setting withdraw manager address by owner

--- test/Crowdsale/withdrawManager.test.js
+++ test/Crowdsale/withdrawManager.test.js
@@ -7,4 +7,9 @@ contract("WitdrawMangerRole", accounts => {
     withdrawManger = await WithdrawManager.new();
   });
 
+  it("should set whitdraw manager by owner", async () => {
+    await withdrawManger.setWithdrawManager(accounts[1], { from: accounts[0] });
+    const manager = await withdrawManger.getWithdrawalManager();
+    assert.equal(manager, accounts[1]);
+  });
 });
