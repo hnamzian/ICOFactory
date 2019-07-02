c55546d173dfebd3895aed9f5a30293c2859bd2a
hnamzian <namazian87@gmail.com>
2019-07-02 14:25:19 +0430

    add onlyWithdrawManager() setWithdrawManager() and getWithdrawManager() from WithdrawaCrowdsale

--- contracts/Crowdsale/WithdrawManagerRole.sol
+++ contracts/Crowdsale/WithdrawManagerRole.sol
@@ -5,4 +5,17 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract WithdrawManagerRole is Ownable {
   address private _withdrawManager;
 
+  modifier onlyWithdrawManager() {
+    require(msg.sender == _withdrawManager, "you are not permitted");
+    _;
+  }
+
+  function setWithdrawManager(address withdrawManager) public onlyOwner {
+    _withdrawManager = withdrawManager;
+  }
+
+  function getWithdrawalManager() public view returns (address) {
+    return _withdrawManager;
+  }
+
 }
