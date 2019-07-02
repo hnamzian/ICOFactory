7ea3dd371ffaa068990396aaf1ace8c88f0222e3
hnamzian <namazian87@gmail.com>
2019-07-02 17:20:20 +0430

    test setting terminator by owner

--- test/Crowdsale/terminatorRole.test.js
+++ test/Crowdsale/terminatorRole.test.js
@@ -6,4 +6,10 @@ contract("WitdrawMangerRole", accounts => {
   beforeEach(async () => {
     terminator = await TerminatorManager.new();
   });
+
+  it("should set terminator address by owner", async () => {
+    await terminator.setTerminator(accounts[1], { from: accounts[0] });
+    const terminatorAddress = await terminator.getTerminator();
+    assert.equal(terminatorAddress, accounts[1]);
+  });
 });
