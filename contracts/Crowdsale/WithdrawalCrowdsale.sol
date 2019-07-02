c5bceb7f3c980931954e0ee18052ebc364772612
hnamzian <namazian87@gmail.com>
2019-07-02 14:32:20 +0430

    import WithdrawalWallet and remove parts related to withdrawal address

--- contracts/Crowdsale/WithdrawalCrowdsale.sol
+++ contracts/Crowdsale/WithdrawalCrowdsale.sol
@@ -2,22 +2,14 @@ pragma solidity "0.5.2";
 
 import "./BasicCrowdsale.sol";
 import "./WithdrawManagerRole.sol";
+import "./WithdrawalWallet.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract WithdrawalCrowdsale is BasicCrowdsale, WithdrawManagerRole {
+contract WithdrawalCrowdsale is 
+  BasicCrowdsale, 
+  WithdrawManagerRole, 
+  WithdrawalWallet {
   using SafeMath for uint256;
-
-  address payable _withdrawalAddress;
-
-  event FundWithdrawn(address _withdrawalAddress, uint256 _fund);
-
-  function setWithdrawalAddress(address payable withdrawalAddress) public onlyOwner {
-    _withdrawalAddress = withdrawalAddress;
-  }
-
-  function getWithdrawalAddress() public view returns (address) {
-    return _withdrawalAddress;
-  }
 
   function withdraw(uint256 fund) public onlyWithdrawManager returns (bool) {
     require(_withdrawalAddress != address(0), "Invalid withdrawal Address");
