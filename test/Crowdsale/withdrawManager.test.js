c0002af746cb6751faeeba49e487624dabf4c8c9
hnamzian <namazian87@gmail.com>
2019-07-02 17:37:31 +0430

    test reversion of setting withdraw manager by address is not owner

--- test/Crowdsale/withdrawManager.test.js
+++ test/Crowdsale/withdrawManager.test.js
@@ -12,4 +12,14 @@ contract("WitdrawMangerRole", accounts => {
     const manager = await withdrawManger.getWithdrawalManager();
     assert.equal(manager, accounts[1]);
   });
+
+  it("should revert setting whitdraw manager by address is not owner", async () => {
+    let revert;
+    try {
+      await withdrawManger.setWithdrawManager(accounts[1], { from: accounts[2] });
+    } catch (ex) {
+      revert = ex.message.includes("revert");
+    }
+    assert.isTrue(revert);
+  });
 });
