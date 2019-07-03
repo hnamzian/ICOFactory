3169f6eca41acab2d7f617a873bf2a5bc0cb12f5
hnamzian <namazian87@gmail.com>
2019-07-03 12:24:27 +0430

    test reversion of setting wallet address by address is not owner

--- test/Crowdsale/withdrawalWallet.test.js
+++ test/Crowdsale/withdrawalWallet.test.js
@@ -12,4 +12,14 @@ contract("WithdrawalWallet", accounts => {
     const walletAddress = await withdrawalWallet.getWithdrawalAddress();
     assert.equal(walletAddress, accounts[1]);
   });
+
+  it("should revert setting withdrawal wallet address by address is not owner", async () => {
+    let revert;
+    try {
+      await withdrawalWallet.setWithdrawalAddress(accounts[1], { from: accounts[2] });
+    } catch (ex) {
+      revert = ex.message.includes("revert");
+    }
+    assert.isTrue(revert);
+  });
 });
