ab3e1e2671321ddf8b1d2cd62c9bce0a0b377eb4
hnamzian <namazian87@gmail.com>
2019-07-03 12:20:58 +0430

    test seting withdrawal wallet address by owner

--- test/Crowdsale/withdrawalWallet.test.js
+++ test/Crowdsale/withdrawalWallet.test.js
@@ -5,6 +5,11 @@ let withdrawalWallet;
 contract("WithdrawalWallet", accounts => {
   beforeEach(async () => {
     withdrawalWallet = await WithdrawalWallet.new();
-  }); 
+  });
 
-})
+  it("should set withdrawal wallet address by owner", async () => {
+    await withdrawalWallet.setWithdrawalAddress(accounts[1], { from: accounts[0] });
+    const walletAddress = await withdrawalWallet.getWithdrawalAddress();
+    assert.equal(walletAddress, accounts[1]);
+  });
+});
